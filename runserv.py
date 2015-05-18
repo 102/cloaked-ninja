2491bfa6e58ea17667980e16d0d533cc4fc0d29b
1 <grdesu@gmail.com>
2015-05-18 10:13:50 +0600

    Add Tornado web-server supporting

---
+++ runserv.py
@@ -0,0 +1,17 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from app import app
+import sys
+
+port = 5000
+try:
+  port = int(sys.argv[1])
+except:
+  pass
+
+print 'running on port {0}'.format(port)
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(port)
+IOLoop.instance().start()
