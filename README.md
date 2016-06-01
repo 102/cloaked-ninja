a651dbc0324db5e9cc809b1a79e445f059d5e564
Roman Gusev <grdesu@gmail.com>
2016-06-01 11:22:40 +0700

    Creating README, LICENSE, remove tabs as indent symbols (#1)
    * Update requirements.txt
    
    * Replace tabs with spaces in script.js
    
    * Create README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# cloaked-ninja
+
+Cloaked ninja is a tool used to upload, make and run some code at remote server.
+
+It was originally developed as a project for university and have been never used in real life.
--- static/script.js
+++ static/script.js
@@ -55,20 +55,18 @@ angular.module('app', ['ngRoute'])
     };
   }])
   .controller('projectsController', ['$scope', '$http', '$window', function($scope, $http, $window) {
-	  $scope.addFile = function(projectName, newFileName) {
-		console.log($scope.newFileName);
-		$http.get('add-file/' + projectName + '/' + (newFileName || 'new file'));
-		$window.location.reload();
-	  }
-      $scope.addProject = function(projectName) {
-		$http.get('add/' + (projectName || 'new project'));
-		$window.location.reload();
-	  }
-	  $scope.runProject = function(projectName, executableName) {
-		console.log('run/' + projectName);
-		$http.post('run/' + projectName, {filename: executableName || 'run'});
-		$window.location.reload();
-	  }
+    $scope.addFile = function(projectName, newFileName) {
+      $http.get('add-file/' + projectName + '/' + (newFileName || 'new file'));
+      $window.location.reload();
+    }
+    $scope.addProject = function(projectName) {
+      $http.get('add/' + (projectName || 'new project'));
+      $window.location.reload();
+    }
+    $scope.runProject = function(projectName, executableName) {
+      $http.post('run/' + projectName, {filename: executableName || 'run'});
+      $window.location.reload();
+    }
   }]);
 
 
